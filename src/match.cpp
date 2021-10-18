52c4a6537220cc94342070be714931e7e31bc9c1
MrQuasar173 <elihatton@zincsoft.dev>
2021-10-17 18:49:19 -0600

    fix search

--- src/match.cpp
+++ src/match.cpp
@@ -2,14 +2,15 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <string>
 
-bool match(std::string inputName, std::string &lineout){
+bool match(const std::string &inputName, std::string &lineout){
     std::ifstream file ("../data/resorts.txt");
     std::string line;
 
     bool match = false;
     while (getline(file, line)){
-        if (line.find(inputName)){
+        if (line.find(inputName) != std::string::npos){
             match = true;
             lineout = line;
             return true;
